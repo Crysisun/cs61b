e528fee40ddc2fb75ddeba1f84f576a54850ed80
Qingyu Sun <sunqingyu1@gmail.com>
2018-06-03 22:04:03 +0800

    hw3 done

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,5 +1,6 @@
 package hw3.hash;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class OomageTestUtility {
@@ -12,6 +13,24 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+
+        HashMap<Integer, Integer> bucketDistribution = new HashMap<>();
+        for(Oomage oomage:oomages){
+            int bucketNum = (oomage.hashCode() & 0x7FFFFFFF) % M;
+            if(bucketDistribution.containsKey(bucketNum)){
+                int count = bucketDistribution.get(bucketNum);
+                bucketDistribution.put(bucketNum, ++count);
+            }else{
+                bucketDistribution.put(bucketNum, 1);
+            }
+        }
+        for(Integer bucketNum:bucketDistribution.keySet()){
+            Boolean niceSpread = (bucketDistribution.get(bucketNum) <= oomages.size() / 2.5)
+                    && (bucketDistribution.get(bucketNum) >= oomages.size() / 50);
+            if(!niceSpread){
+                return false;
+            }
+        }
+        return true;
     }
 }
