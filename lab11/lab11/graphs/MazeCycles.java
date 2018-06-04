75c587c3d7b64a90bc07577d1de521a67e1996c3
Qingyu Sun <sunqingyu1@gmail.com>
2018-06-04 19:40:36 +0800

    proj11 done

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,14 +9,23 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(0,0);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+
     }
 
     // Helper methods go here
